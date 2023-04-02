d563f5d86cdbfcb39d573877ba0111e4a6a20cfc
ch <mkmsacc@outlook.com>
2023-04-02 15:27:16 +0700

    Tidy

--- learnreactiveprogramming/src/main/java/org/example/service/FluxService.java
+++ learnreactiveprogramming/src/main/java/org/example/service/FluxService.java
@@ -5,19 +5,21 @@ import reactor.core.publisher.Flux;
 import java.util.List;
 
 public class FluxService {
+    private final List<String> names = List.of("Alex", "Ben", "Cathy");
+
     public Flux<String> namesFlux() {
-        return Flux.fromIterable(List.of("Alex", "Ben", "Cathy")).log();
+        return Flux.fromIterable(names).log();
     }
 
     public Flux<String> namesFluxMap() {
-        return Flux.fromIterable(List.of("Alex", "Ben", "Cathy"))
+        return Flux.fromIterable(names)
                 .map(String::toUpperCase)
                 .log();
     }
 
     public Flux<String> namesFluxMapImmutability() {
 
-        Flux<String> stringFlux = Flux.fromIterable(List.of("Alex", "Ben", "Cathy"));
+        Flux<String> stringFlux = Flux.fromIterable(names);
 //        stringFlux.map(String::toUpperCase); // immutability
 
         return stringFlux.map(String::toUpperCase).log();
