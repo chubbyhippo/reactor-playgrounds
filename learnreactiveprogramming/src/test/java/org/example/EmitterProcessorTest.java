61dd8816e4ced479b1947186db1be76864c2b23a
ch <mkmsacc@outlook.com>
2023-04-03 14:58:15 +0700

    Implement should emit processor

---
+++ learnreactiveprogramming/src/test/java/org/example/EmitterProcessorTest.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
+import reactor.test.StepVerifier;
+
+class EmitterProcessorTest {
+
+    @Test
+    void shouldEmitProcessor() {
+
+        Sinks.Many<String> sink = Sinks.many().multicast().onBackpressureBuffer();
+        sink.tryEmitNext("a");
+        sink.tryEmitNext("b");
+        sink.tryEmitComplete();
+
+        Flux<String> flux = sink.asFlux().log();
+
+        StepVerifier.create(flux)
+                .expectNext("a")
+                .expectNext("b")
+                .verifyComplete();
+
+        StepVerifier.create(flux)
+                .verifyComplete();
+    }
+
+}
